fb85d05860ecf842673b901f06f0a18b07ef077e
Juan Pablo <juandeooliveira@gmail.com>
2026-05-06 09:35:02 -0300

    feat(backend): Adiconad as rotas de emotions.py no arquivo main.py central

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from app.routes.health import router as health_router
 from app.routes.users import router as users_router
 from app.routes.vectors import router as vectors_router
+from app.routes.emotions import router as emotions_router
 
 app = FastAPI(title="API Projeto faculdade", version="0.1.0")
 
 app.include_router(health_router)
 app.include_router(users_router)
 app.include_router(vectors_router)
+app.include_router(emotions_router)
 
 @app.get("/")
 def root():
